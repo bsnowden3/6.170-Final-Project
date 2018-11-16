9bf317389e4fe736776dfbc657a2e4dcff273ff4
prateekkuk <prateek.kukreja@gmail.com>
2018-11-15 22:45:33 -0500

    trying again

---
+++ lively/README.md
@@ -0,0 +1,14 @@
+# URL Shortener
+A simple URL shortener made with Node, Express and Vue.js for Recitation 4 of Fall 2018 6.170 Software Studio.
+
+## Installation
+```
+npm install
+```
+
+## Usage
+```
+npm run build
+npm run start
+```
+Navigate to `localhost:3000` in your browser and start creating shorts!
---
+++ lively/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app" class=''>
+    <h1>Lively</h1>
+    <!-- Children Components -->
+    <CreateShortForm/>
+    <ShortList/>
+  </div>
+</template>
+
+<script>
+import CreateShortForm from "./components/CreateShortForm.vue";
+import ShortList from "./components/ShortList.vue";
+
+export default {
+  name: "app",
+  components: {
+    CreateShortForm,
+    ShortList
+  }
+};
+</script>
+
+<!-- global styles -->
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: lightblue;
+}
+
+.success-message {
+  color: green;
+}
+
+.error-message {
+  color: red;
+}
+
+.component {
+  background-color: whitesmoke;
+  padding: 1rem;
+}
+</style>
+
+<style scoped>
+  .horiz-center {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
