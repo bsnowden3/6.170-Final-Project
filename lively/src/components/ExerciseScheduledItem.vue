bc32c1cdf65ad4acca2f1d51cd0ff155e49775dc
bsnowden3 <bsnowden@mit.edu>
2018-11-20 02:29:07 -0500

    edit list items

---
+++ lively/src/components/ExerciseScheduledItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="short-list-item component">
+    <p>Name: {{ exercise.name }}</p>
+    <p>mealId: {{ exercise.mealId }}</p>
+    <p>Start Time: {{ exercise.startTime }}</p>
+    <p>End Time: {{ exercise.endTime }}</p>
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line
+import axios from "axios";
+// eslint-disable-next-line
+import { eventBus } from "../main";
+
+export default {
+  name: "ExerciseScheduledItem",
+
+  props: {
+    exercise: []
+  },
+};
+</script>
---
+++ lively/src/components/SleepScheduledItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="short-list-item component">
+    <p>Name: {{ short.name }}</p>
+    <p>URL: {{ short.url }}</p>
+    <p>Created by: {{ creatorIdentity }}</p>
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line
+import axios from "axios";
+// eslint-disable-next-line
+import { eventBus } from "../main";
+
+export default {
+  name: "SleepScheduledItem",
+
+  props: {
+    short: Object
+  },
+
+  computed: {
+    creatorIdentity: function() {
+      return this.short.creator ? this.short.creator : "anonymous";
+    }
+  }
+};
+</script>
